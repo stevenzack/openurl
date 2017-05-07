c3e83fdc0812229d067f5865af94cf66efb35cd3
Steven Zack <zigzigcheers@gmail.com>
2017-05-07 19:19:58 +0800

    Update openurl.go

--- openurl.go
+++ openurl.go
@@ -20,10 +20,10 @@ func Open(url string) error {
 	}
 	switch runtime.GOOS {
 	case "windows":
-		if _, err := os.Stat("\"C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Google Chrome\""); !os.IsNotExist(err) { //Chrome  installed on this computer
-			exec.Command("cmd", "/C", "copy", "C:\\ProgramData\\Microsoft\\Windows\\StartM~1\\Programs\\Google Chrome.lnk", ".\\GC.lnk").Run()
-			return exec.Command("cmd", "/C", "GC.lnk","--app="+url).Start()
-		}
+		//if _, err := os.Stat("\"C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Google Chrome\""); !os.IsNotExist(err) { //Chrome  installed on this computer
+		//	exec.Command("cmd", "/C", "copy", "C:\\ProgramData\\Microsoft\\Windows\\StartM~1\\Programs\\Google Chrome.lnk", ".\\GC.lnk").Run()
+		//	return exec.Command("cmd", "/C", "GC.lnk","--app="+url).Start()
+		//}
 		return exec.Command("explorer", url).Start()
 	case "darwin":
 		if err := exec.Command("google-chrome", "--app="+url).Start(); err != nil {
